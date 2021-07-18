abb76d369f685af88a05afac60cc7df2fc940a07
Sharifdesigns22 <shariftarver@gmail.com>
2021-07-18 10:44:30 -0400

    commit tree 55e712b12dc6b32017b3705932a68ce8053451a7

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,18 @@ class BirdsController < ApplicationController
     @birds = Bird.all
     render json: @birds
   end
+
+  def show
+    # To also remove all instances of :created_at and :updated_at from the nested bird 
+    # and location data we'd have to add nesting into the options, so the 
+    # included bird and location data can have their own options listed. Using the fully written to_json 
+    # render statement can help keep things more readable here:
+
+    sighting = Sighting.find_by(id: params[:id])
+    render json: sighting.to_json(:include => {
+      :bird => {:only => [:name, :species]},
+      :location => {:only => [:latitude, :longitude]}
+    }, :except => [:updated_at])
+
+  end
 end
